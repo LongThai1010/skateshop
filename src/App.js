6e649c247c82a1e06005be346910ef5d269d9fcc
long thai <volelongthaipnt@gmail.com>
2023-09-28 08:32:06 +0700

    push project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import "./Responsive.css";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import OurStore from "./pages/OurStore";
+import WishList from "./pages/WishList";
+import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
+import Signup from "./pages/Signup";
+import ResetPassword from "./pages/ResetPassword";
+import SingleProduct from "./pages/SingleProduct";
+import Cart from "./pages/Cart";
+import CheckOut from "./pages/Checkout";
+import { PrivateRoute } from "./routing/PrivateRoute";
+import { OpenRoute } from "./routing/OpenRoute";
+import Profile from "./pages/Profile";
+import Orders from "./pages/Orders";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="product" element={<OurStore />} />
+            <Route
+              path="cart"
+              element={
+                <>
+                  <Cart />
+                </>
+              }
+            />
+            <Route
+              path="my-orders"
+              element={
+                <PrivateRoute>
+                  <Orders />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="my-profile"
+              element={
+                <PrivateRoute>
+                  <Profile />
+                </PrivateRoute>
+              }
+            />
+            <Route path="checkout" element={<CheckOut />} />
+            <Route
+              path="signup"
+              element={
+                <OpenRoute>
+                  <Signup />
+                </OpenRoute>
+              }
+            />
+            <Route
+              path="login"
+              element={
+                <OpenRoute>
+                  <Login />
+                </OpenRoute>
+              }
+            />
+            <Route path="product/:id" element={<SingleProduct />} />
+            <Route
+              path="wishlist"
+              element={
+                <>
+                  <WishList />
+                </>
+              }
+            />
+            <Route path="forgot-password" element={<ForgotPassword />} />
+            <Route path="reset-password/:token" element={<ResetPassword />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 

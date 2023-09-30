75f4b46a8cd0aeb3ef1fa8ac3a8182d0fd907000
long thai <volelongthaipnt@gmail.com>
2023-09-30 20:20:34 +0700

    changesUrl

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,4 +1,4 @@
-export const base_url = "http://localhost:5000/api/";
+export const base_url = "https://api-stakeshop.vercel.app/api/";
 
 const getTokenFromLocalStorage = localStorage.getItem("customer")
   ? JSON.parse(localStorage.getItem("customer"))
